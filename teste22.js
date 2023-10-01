2b30025703a05fb1bc1aa4b6e9b68a7ff7fcb662
Alisson Antonio <111544650+Alisson42Alves@users.noreply.github.com>
2023-10-01 02:14:52 -0300

    Create teste22.js

---
+++ teste22.js
@@ -0,0 +1,21 @@
+/***** Não altere os códigos dentro desse bloco *****/
+let senhaDoWiFi = 159000;
+
+let senhaDigitada1 = 169000;
+let senhaDigitada2 = 159000;
+let senhaDigitada3 = '159000';
+let senhaDigitada4 = 159000;
+/***** Não altere os códigos dentro desse bloco *****/
+
+/***** Escreva seu código nas linhas abaixo *****/
+
+let confirmarSenha1 = senhaDigitada1 === senhaDoWiFi;
+let confirmarSenha2 =  senhaDigitada2 === senhaDoWiFi;
+let confirmarSenha3 = senhaDigitada3 === senhaDoWiFi;
+let confirmarSenha4 = senhaDigitada4 === senhaDoWiFi;
+
+console.log(confirmarSenha1);
+console.log(confirmarSenha2);
+console.log(confirmarSenha3);
+console.log(confirmarSenha4);
+
